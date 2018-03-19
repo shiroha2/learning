d3ecbd0ff723283ac33039b246fbf8e561f50756
Supakit Sujaritcheewawong <supakit021@gmail.com>
2018-03-19 15:39:51 +0700

    and types userActions

---
+++ app/actions/userAction.js
@@ -0,0 +1,28 @@
+import firebase from 'firebase'
+import {
+    USER_CREATE,
+    USER_UPDATE,
+} from '../type/types'
+
+export const userUpdate = ({prop , value}) => {
+    return {
+        type: user_UPDATE,
+        payload: {prop , value}
+    }
+}
+
+
+export const userCreate =  ({name , email ,status}) => {
+    console.log(name, email ,status)
+    const { currentUser } = firebase.auth();
+ 
+    const user = firebase.database().ref(`/users/${currentUser.uid}/student`);
+ 
+    return (dispatch) => {
+        employees.push({ name, phone, status })
+        .then(() => {
+            dispatch({ type: USER_CREATE });
+            Actions.employeeList({ type: 'reset' });
+        });
+    };
+}
---
+++ app/type/types.js
@@ -0,0 +1,2 @@
+export const USER_CREATE = 'user_create'
+export const USER_UPDATE = 'user_update'
