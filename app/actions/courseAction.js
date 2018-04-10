fcf1caf30ef7495765fc287678a412fc7e7bdd5a
Supakit Sujaritcheewawong <supakit021@gmail.com>
2018-04-10 15:32:49 +0700

    add google-services.json and courseAction.js and Login Form.js

---
+++ app/actions/courseAction.js
@@ -0,0 +1,8 @@
+import firebase from 'firebase'
+
+import {
+    COURSE_UPDATE,
+    COURSE_CREATE,
+    COURSE_DELETE
+} from '../type/types'
+
---
+++ app/auth/loginForm.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { View, Text, Button, TextInput } from 'react-native'
+
+class LoginForm extends Component{
+  state = {
+    email: '',
+    password: ''
+  }
+    render(){
+      return (
+
+        <Button
+          title='Log in'
+
+        />
+      )
+    }
+}
